b85b2c758b57d2b8e5430c74d8e80c17cb1aca75
Dilusha Sandeera Jayampathi <32511955+SandeeraDS@users.noreply.github.com>
2023-01-21 19:04:58 +0530

    initial

---
+++ src/main/java/com/gtn/oms/owasptest/OwsapTestApplication.java
@@ -0,0 +1,13 @@
+package com.gtn.oms.owasptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwsapTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwsapTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gtn/oms/owasptest/OwsapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtn.oms.owasptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwsapTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
